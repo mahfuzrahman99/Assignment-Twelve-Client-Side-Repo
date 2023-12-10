b0cd136f2027698044601fdee9611cff7eed70e0
mahfuzurrahman99 <mahfuzurrahmanshabbir@gmail.com>
2023-12-10 10:39:30 +0600

    Assignment Twelve Client Side

--- src/Pages/Organizetion Owner/OrganizationOwner.jsx
+++ src/Pages/Organizetion Owner/OrganizationOwner.jsx
@@ -20,13 +20,12 @@ const OrganizationOwner = () => {
         <div className="col-span-1 text-md p-4 md:text-2xl font-semibold text-gray-500 space-y-3 md:space-y-8">
           <p>
             Hi, my name is Jeffrey, and I’m 23 years of age. I live in The
-            Netherlands, as I have for my entire life. I am the writer of the
-            articles published on Your Dream Coffee.
+            Netherlands, as I have for my entire life. I am organizer of your dream
+            medical camp management CareCampusPro.
           </p>
           <p>
             A few years back, I finished culinary school and have been working
-            in the kitchen ever since. This is where I found out I like drinking
-            coffee and haven’t stopped since.
+            in the kitchen ever since. This is where I found out I would like to help others  .
           </p>
           <p className="bg-clip-text text-transparent bg-gradient-to-l from-red-400 to-blue-400 font-bold">
             Learn more about me
